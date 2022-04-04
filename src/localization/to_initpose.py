23501693eeba5eee556197dd33af38bea05dd69d
ArcGomez <arc.rafael.gomez@gmail.com>
2022-04-04 19:42:10 +0000

    to initi pose

--- src/localization/to_initpose.py
+++ src/localization/to_initpose.py
@@ -7,18 +7,17 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 
 def poster():
     pub = rospy.Publisher("/initialpose",PoseWithCovarianceStamped,queue_size=1)
+    rospy.init_node('initpose')
     
     ipose=PoseWithCovarianceStamped()
-
-    ipose.header.stamp=rospy.Time.now()
     ipose.header.frame_id="map"
-
-    ipose.pose.pose.position.x=-11.30141
-    ipose.pose.pose.position.y=13.85292625
+    ipose.pose.pose.position.x=-21.47825
+    ipose.pose.pose.position.y=1.5059
     ipose.pose.pose.position.z=0
 
-    ipose.pose.pose.orientation.z=-0.91244
-    ipose.pose.pose.orientation.w=0.409212
+    ipose.pose.pose.orientation.z=0.6055
+    ipose.pose.pose.orientation.w=0.7958
+    ipose.pose.covariance=[0.25,0,0,0,0,0,0,0.25,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.0685389]
     pub.publish(ipose)
 
 
@@ -30,4 +29,4 @@ def poster():
 
 
 if __name__ =='__main__':
-    talker()
+    poster()
